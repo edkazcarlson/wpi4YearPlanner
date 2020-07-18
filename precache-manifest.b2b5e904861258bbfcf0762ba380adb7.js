23bfaa9a8f5ed73b40675abe44759175bfc17406
plupiman <kazuya@comcast.net>
2020-07-18 14:43:14 -0700

    Updates

---
+++ precache-manifest.b2b5e904861258bbfcf0762ba380adb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1137712ebd4ebcb5c4f1abd89b11b5",
+    "url": "/edkazcarlson.github.io/4YearPlanner/index.html"
+  },
+  {
+    "revision": "f060276a39d9b1e68f74",
+    "url": "/edkazcarlson.github.io/4YearPlanner/static/css/main.ed989b49.chunk.css"
+  },
+  {
+    "revision": "96c3522196a429bbb3bb",
+    "url": "/edkazcarlson.github.io/4YearPlanner/static/js/2.9ca27b1e.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/edkazcarlson.github.io/4YearPlanner/static/js/2.9ca27b1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f060276a39d9b1e68f74",
+    "url": "/edkazcarlson.github.io/4YearPlanner/static/js/main.f395d712.chunk.js"
+  },
+  {
+    "revision": "7244e553f7908fe37c34",
+    "url": "/edkazcarlson.github.io/4YearPlanner/static/js/runtime-main.a5063dc5.js"
+  }
+]);
